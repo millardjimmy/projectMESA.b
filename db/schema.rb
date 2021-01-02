d28882ae3c9e8634fbdb12d9d619d919966fbf0b
millardjimmy <millardjimmy@gmail.com>
2021-01-02 11:47:25 -0700

    add usernames to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_175953) do
+ActiveRecord::Schema.define(version: 2021_01_02_184424) do
 
   create_table "boxes", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_01_02_175953) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
