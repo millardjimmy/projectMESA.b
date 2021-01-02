a9c95af97b721784c57491b09af08eade577b628
millardjimmy <millardjimmy@gmail.com>
2021-01-02 11:53:35 -0700

    add post|get routes for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users do   
+  resources :users do
+    post '/login', to: 'auth#create'
+    get '/profile', to: 'users#profile'   
     resources :moves do
       get 'items' => 'items#allItemsInAMove'
       resources :boxes do 
