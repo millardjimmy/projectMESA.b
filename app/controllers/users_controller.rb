64ff08f0ded88aacad10d6c226ca31424299a9fe
millardjimmy <millardjimmy@gmail.com>
2021-01-02 12:00:25 -0700

    buildout for authentication - JWT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show]
+  skip_before_action :authorized, only: [:create]
+  #before_action :find_user, only: [:show]
 
  def index
    @users = User.all
@@ -14,38 +15,16 @@ class UsersController < ApplicationController
     render json: @user, status: :ok
   end
 
-  #  WITH AUTH:
-  # def create
-  #   @user = User.create(user_params)
-  #   if @user.valid?
-  #     # @token = encode_token(user_id: @user.id)
-  #     render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-  #   else
-  #     render json: { error: 'failed to create user' }, status: :not_acceptable
-  #   end
-  # end
-
-  # READ ME FOR AUTH
+  
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
-
-  # WITHOUT AUTH
- #  def create
- #   @user = User.new(user_params)
- #   # byebug
- #   if @user.valid?
- #     @user.save
- #     render json: @user, status: :accepted
- #   else
- #     render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
- #   end
- # end
 
  def update
    @user.update(user_params)
