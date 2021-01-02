60e0436676a7762c27cf6d67740b12e6b212040b
millardjimmy <millardjimmy@gmail.com>
2021-01-02 11:33:25 -0700

    added a route to allow user to search items

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users do
-        resources :moves do
-          resources :boxes do 
-            resources :items do
-            end
-          end
+  resources :users do   
+    resources :moves do
+      get 'items' => 'items#allItemsInAMove'
+      resources :boxes do 
+        resources :items do
         end
       end
     end
