382fce9bf8a4cc3a4e95b11b1e1ec1607c23b69d
millardjimmy <millardjimmy@gmail.com>
2021-01-02 11:53:09 -0700

    add authentication to user model|controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,75 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, only: [:show]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
+ def index
+   @users = User.all
+   render json: @users
+ end
+
+ def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
+ def show
+    render json: @user, status: :ok
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
+  #  WITH AUTH:
+  # def create
+  #   @user = User.create(user_params)
+  #   if @user.valid?
+  #     # @token = encode_token(user_id: @user.id)
+  #     render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+  #   else
+  #     render json: { error: 'failed to create user' }, status: :not_acceptable
+  #   end
+  # end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
+  # READ ME FOR AUTH
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # WITHOUT AUTH
+ #  def create
+ #   @user = User.new(user_params)
+ #   # byebug
+ #   if @user.valid?
+ #     @user.save
+ #     render json: @user, status: :accepted
+ #   else
+ #     render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+ #   end
+ # end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+ def update
+   @user.update(user_params)
+   if @user.save
+     render json: @user, status: :accepted
+   else
+     render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+   end
+ end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+ # def destroy
+ #   @user.destroy
+ # end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name)
-    end
+ private
+
+ # # TODO: when i add auth, will have to add username and password
+ # password_digest to the schema though
+ def user_params
+   params.require(:user).permit(:name, :username, :password)
+ end
+
+ def find_user
+   @user = User.find(params[:id])
+ end
+
 end
